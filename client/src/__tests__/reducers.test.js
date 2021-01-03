248cdf09358fa25fcadd03a3bfe96946fb5624a6
daejo <daejo@icloud.com>
2021-01-02 19:55:23 -0800

    updates reducer and test files

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,4 +1,15 @@
 import { reducer } from '../utils/reducers';
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
+} from '../utils/actions';
 
 // import our actions
 import {
@@ -42,4 +53,33 @@ import {
   
     expect(newState.currentCategory).toBe('2');
     expect(initialState.currentCategory).toBe('1');
+  });
+
+  const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+    cart: [
+      {
+        _id: '1',
+        name: 'Soup',
+        purchaseQuantity: 1
+      },
+      {
+        _id: '2',
+        name: 'Bread',
+        purchaseQuantity: 2
+      }
+    ],
+    cartOpen: false
+  };
+
+  test('ADD_TO_CART', () => {
+    let newState = reducer(initialState, {
+      type: ADD_TO_CART,
+      product: { purchaseQuantity: 1 }
+    });
+  
+    expect(newState.cart.length).toBe(3);
+    expect(initialState.cart.length).toBe(2);
   });
